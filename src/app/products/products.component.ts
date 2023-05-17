7f4de1261e73fb53faff6b68f774d4568538c697
razieh <razieh20.com@gmail.com>
2023-05-17 08:54:50 -0400

    search box was added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -14,7 +14,12 @@ export class ProductsComponent implements OnInit{
   inputValue: string = "";
   processedValue: any;
   des: any
+  is_default: boolean = true
   search_result: any = []
+  description_list:any = []
+  name_list: any = []
+  category_list: any = []
+
   constructor(private api: ApiService, private cartService: CartService){
 
   }
@@ -22,12 +27,10 @@ export class ProductsComponent implements OnInit{
   ngOnInit(): void {
     this.api.getProduct().subscribe( res => {
       this.productList = res
-      this.productList.reverse()
-
-      this.productList.forEach((a: any) => {
-        Object.assign(a, {quantity: 1, total: a.price})      
-      });
+      this.set_data_lists()
     })
+
+    this.set_search_default()
   }
 
   public productList:any
@@ -37,14 +40,50 @@ export class ProductsComponent implements OnInit{
   }
 
   processInput() {
+    this.is_default = false
     this.processedValue = this.inputValue;
     console.log(this.processedValue, '#val')
 
-    this.productList.forEach((res: any) => {
-      if(res.description.includes(this.processedValue)) {
-        this.search_result = res
+    this.set_search_result()
+  }
+
+  set_search_result() {
+    for (let i = 0; i < this.search_result.length ; i++) {
+      this.search_result[i] = null
+    }
+
+    for (let i = 0; i < this.productList.length ; i++) {
+      if(this.description_list[i].toLowerCase().includes(this.processedValue.toLowerCase()) ||
+         this.name_list[i].toLowerCase().includes(this.processedValue.toLowerCase()) ||
+         this.category_list[i].toLowerCase().includes(this.processedValue.toLowerCase())){
+         this.search_result[i] = this.productList[i]
       }
+    }
+
+    console.log(this.search_result, '#search_result')
+  }
+
+  set_data_lists(){
+    for (let i = 0; i < this.productList.length ; i++) {
+      this.description_list[i] = this.productList[i].description
+      this.name_list[i] = this.productList[i].title
+      this.category_list[i] = this.productList[i].category 
+    }
+
+    this.productList.reverse()
+    this.description_list.reverse()
+    this.name_list.reverse()
+    this.category_list.reverse()
+
+    this.productList.forEach((a: any) => {
+      Object.assign(a, {quantity: 1, total: a.price})
     });
   }
 
+  set_search_default() {
+    if(this.is_default == true)
+    for (let i = 0; i < this.productList.length ; i++) {
+      this.search_result[i] = this.productList[i]
+    }
+  }
 }
